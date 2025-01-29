393cd1e6eb74c8d74a26fac4a6afefc07b75b267
Burhan Anis <burhanuddin.m.anis@gmail.com>
2025-01-29 13:23:38 +0530

    update port

--- api.py
+++ api.py
@@ -9,7 +9,7 @@ def home():
     return "API is running!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8080)
 
 topic_labels_store = {}
 topic_results_store = {}
